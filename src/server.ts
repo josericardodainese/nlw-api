4303e18240371ef792562a98461885fe44745e73
Ze <josericardodainese@gmail.com>
2020-08-10 02:34:27 -0300

    NLW Finalizado API

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,12 @@
 import  express from 'express';
-
+import routes from './routes';
+import cors from 'cors';
 
 const app = express();
 
-app.use(express.json())
+app.use(cors);
+app.use(express.json());
+app.use(routes);
 
-app.get('/', (request, response) => {
-    return response.json({message:"hello world"});
-})
 
 app.listen(3333);
