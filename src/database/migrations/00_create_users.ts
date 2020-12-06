96033859d4faad721de7bcbaab520680e7b2e548
Ze <josericardodainese@gmail.com>
2020-12-06 18:02:43 -0300

    WIP

--- src/database/migrations/00_create_users.ts
+++ src/database/migrations/00_create_users.ts
@@ -5,6 +5,7 @@ export async function up(knex: knex) {
     return knex.schema.createTable('users', table => {
         table.increments('id').primary();
         table.string('name').notNullable();
+        table.string('email').notNullable();
         table.string('avatar').notNullable();
         table.string('whatsapp').notNullable();
         table.string('bio').notNullable();
--- src/server.ts
+++ src/server.ts
@@ -4,9 +4,8 @@ import cors from 'cors';
 
 const app = express();
 
-app.use(cors);
+app.use(cors());
 app.use(express.json());
 app.use(routes);
-
 
 app.listen(3333);
